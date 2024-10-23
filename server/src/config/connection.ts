1f54349c7dae27493c7cbbccd2c17cc947190057
KYLE VANCE <vanceofalifetime@protonmail.com>
2024-10-22 18:15:20 -0700

    sdf

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,19 +1,19 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-// const MONGODB_URI = process.env.MONGODB_URI || '';
-const MONGODB_URI = "mongodb+srv://kvance:password1234@cluster0.ugwti.mongodb.net/wordGuessingGame?retryWrites=true&w=majority&appName=Cluster0";
+const MONGODB_URI = process.env.MONGODB_URI || "";
+// const MONGODB_URI = "mongodb+srv://kvance:password1234@cluster0.ugwti.mongodb.net/wordGuessingGame?retryWrites=true&w=majority&appName=Cluster0";
 
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
     await mongoose.connect(MONGODB_URI);
-    console.log('Database connected.');
+    console.log("Database connected.");
     return mongoose.connection;
   } catch (error) {
-    console.error('Database connection error:', error);
-    throw new Error('Database connection failed.');
+    console.error("Database connection error:", error);
+    throw new Error("Database connection failed.");
   }
 };
 
